12dfd4f9a08cc828965e8e988579a3a87ff8e664
Ruta Jogani <ruta007jogani@gmail.com>
2025-07-07 22:40:05 +0530

    add: some code in js file

--- register/register.js
+++ register/register.js
@@ -1,3 +1,11 @@
 function register() {
-    
+    const email = document.getElementById("email");
+    const register_password = document.getElementById("password");
+    const confirm_password = document.getElementById("confirm");
+
+    if (register === confirm) {
+        localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(register_password,confirm_password,email));
+    }else{
+        alert("Confirm The Password");
+    }
 }
