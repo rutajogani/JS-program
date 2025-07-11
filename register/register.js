8fbcfdd0e15104f847b8192cd5a8527f6b6a3b1c
Ruta Jogani <ruta007jogani@gmail.com>
2025-07-11 21:14:44 +0530

    add more lines in js

--- register/register.js
+++ register/register.js
@@ -1,23 +1,25 @@
 const LOCAL_STORAGE_KEY = "register";
 
-function register() {
+function register(event) {
+  event.preventDefault();
+
   const email = document.getElementById("email");
   const password = document.getElementById("password");
   const confirmPassword = document.getElementById("confirm");
 
   if (password.value === confirmPassword.value) {
+    console.log("wr");
+
     localStorage.setItem(
       LOCAL_STORAGE_KEY,
       JSON.stringify({
         email: email.value,
-        password: password.value
+        password: password.value,
       })
     );
+
+    window.location.href = "/login";
   } else {
     alert("Confirm password is not same!");
   }
-  const button = document.getElementById("btn");
-  button.addEventListener("click", () => {
-     window.location.href = "../login.html"; 
-  })
 }
