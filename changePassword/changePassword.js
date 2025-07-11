10ffd1e3668f528c8a4e633208e3170b22e338de
Ruta Jogani <ruta007jogani@gmail.com>
2025-07-11 22:28:01 +0530

    add: more lines

---
+++ changePassword/changePassword.js
@@ -0,0 +1,10 @@
+function changePassword(event) {
+  event.preventDefault();
+  const oldPassword = document.getElementById("old");
+  const newPassword = document.getElementById("new");
+
+  if (oldPassword.value === w) {
+  } else {
+    alert("Old password is not same!");
+  }
+}
