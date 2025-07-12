af5bf6ecb93bef4e15abff41f899c67fa516023e
Ruta Jogani <ruta007jogani@gmail.com>
2025-07-12 22:48:51 +0530

    add more liens

--- changePassword/changePassword.js
+++ changePassword/changePassword.js
@@ -1,10 +1,26 @@
-function changePassword(event) {
-  event.preventDefault();
-  const oldPassword = document.getElementById("old");
-  const newPassword = document.getElementById("new");
-
-  if (oldPassword.value === w) {
-  } else {
-    alert("Old password is not same!");
+const LOCAL_STORAGE_KEY = "register";
+const button = document.getElementById("btn");
+button.addEventListener("click", ()=>{
+  changePassword()  
+})
+  
+  function changePassword(event) {
+    event.preventDefault();
+    
+    const oldPassword = document.getElementById("old");
+    const newPassword = document.getElementById("new");
+    
+    const local = localStorage.setItem(
+      LOCAL_STORAGE_KEY,
+      JSON.stringify({
+        email: email.value,
+      })
+    );
+    
+    if (oldPassword.value === local.value) {
+      window.location.href = "/login";
+    } else {
+      alert("Old password is not same!");
+    }
   }
-}
+  
